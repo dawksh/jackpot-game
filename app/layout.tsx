3c365814f6a2093c5865e30b80e16cbad5270a09
daksh <akshd@outlook.com>
2025-02-08 10:26:43 +0530

    chore: update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Jackpot Game',
+  description: 'Play and earn with Jackpot Game!',
 }
 
 export default function RootLayout({
@@ -14,6 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <meta name="fc:frame" content='{"version":"next","imageUrl":"https://yoink.party/img/start.png","button":{"title":"Win $JACKPOT","action":{"type":"launch_frame","name":"JACKPOT","url":"https://yoink.party/","splashImageUrl":"https://yoink.party/img/splash.png","splashBackgroundColor":"#eeeee4"}}}' />
       </head>
       <body>{children}</body>
