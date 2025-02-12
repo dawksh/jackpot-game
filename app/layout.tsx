aa70bf0c6a45b7d44ea8ad76d47683441b5539f3
daksh <akshd@outlook.com>
2025-02-13 03:21:49 +0530

    chore: remove frame

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,6 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        <meta name="fc:frame" content='{"version":"next","imageUrl":"https://yoink.party/img/start.png","button":{"title":"Win $JACKPOT","action":{"type":"launch_frame","name":"JACKPOT","url":"https://yoink.party/","splashImageUrl":"https://yoink.party/img/splash.png","splashBackgroundColor":"#eeeee4"}}}' />
       </head>
       <body>{children}</body>
     </html>
