c3e04d7295521006344045725eb30ab53acbaa88
daksh <akshd@outlook.com>
2025-02-23 00:09:27 +0530

    chore: update to backend sfor signature

--- app/api/signature/route.ts
+++ app/api/signature/route.ts
@@ -1,59 +1,18 @@
+import axios from "axios";
 import { NextResponse, NextRequest } from "next/server";
-import { createWalletClient, http } from "viem";
-import { privateKeyToAccount } from "viem/accounts";
-import { base } from "viem/chains";
 
-// To handle a GET request to /api
 export async function GET(request: NextRequest) {
-    // Do whatever you want
     const amount = request.nextUrl.searchParams.get("amount")
     const winner = request.nextUrl.searchParams.get("winner")
-    const signature = await genSig(BigInt(amount as string), winner as string)
+    const { data: { signature } } = await axios.get("http://127.0.0.1:3000/signature", {
+        headers: {
+            "Authorization": "Bearer 5ed1d456-43fe-4921-8007-bb2adc957d6a"
+        },
+        params: {
+            amount: amount?.toString(),
+            winner
+        }
+    })
     return NextResponse.json({ signature }, { status: 200 });
 }
 
-const genSig = async (amount: BigInt, winner: string) => {
-    const CLAIM_CONTRACT_ADDRESS: `0x${string}` =
-        "0xcb8F593526Ef882a153CfD80D17DbBB9576CcF7c";
-    const chainId = 8453;
-    const domainName = "JackpotGameStore";
-    const domainVersion = "1";
-
-    const account = privateKeyToAccount(
-        process.env.NEXT_PUBLIC_PRIVATE_KEY as `0x${string}`
-    );
-
-    const client = createWalletClient({
-        account,
-        chain: base,
-        transport: http(),
-    });
-
-    const domain = {
-        name: domainName,
-        version: domainVersion,
-        chainId,
-        verifyingContract: CLAIM_CONTRACT_ADDRESS,
-    };
-
-    const types = {
-        claim: [
-            { name: "winner", type: "address" },
-            { name: "amount", type: "uint256" },
-        ],
-    };
-
-    const message = {
-        winner: winner || account.address,
-        amount,
-    };
-
-    const signature = await client.signTypedData({
-        domain,
-        types,
-        primaryType: "claim",
-        message,
-    });
-
-    return signature;
-};
