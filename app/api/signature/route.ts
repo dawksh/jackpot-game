a60d6b9914b489e37c9d4eb94f814aa9a0121dee
daksh <akshd@outlook.com>
2025-02-23 00:30:25 +0530

    chore: update url

--- app/api/signature/route.ts
+++ app/api/signature/route.ts
@@ -4,9 +4,9 @@ import { NextResponse, NextRequest } from "next/server";
 export async function GET(request: NextRequest) {
     const amount = request.nextUrl.searchParams.get("amount")
     const winner = request.nextUrl.searchParams.get("winner")
-    const { data: { signature } } = await axios.get("http://127.0.0.1:3000/signature", {
+    const { data: { signature } } = await axios.get("https://jackpot-backend.up.railway.app/signature", {
         headers: {
-            "Authorization": "Bearer 5ed1d456-43fe-4921-8007-bb2adc957d6a"
+            "Authorization": `Bearer ${process.env.API_KEY}`
         },
         params: {
             amount: amount?.toString(),
