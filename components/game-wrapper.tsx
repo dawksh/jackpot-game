d352b9015b06b1a7545c5b2671278cb29c17df4d
daksh <akshd@outlook.com>
2025-03-08 15:48:52 +0530

    feat: add context

--- components/game-wrapper.tsx
+++ components/game-wrapper.tsx
@@ -2,21 +2,27 @@
 
 import JackpotGame from '@/jackpot-game'
 import React, { useEffect, useState } from 'react'
-import sdk from "@farcaster/frame-sdk";
+import sdk, { Context } from "@farcaster/frame-sdk";
 
 const GameWrapper = () => {
     const [isSDKLoaded, setIsSDKLoaded] = useState(false);
+    const [context, setContext] = useState<Context.FrameContext>();
 
     useEffect(() => {
         const load = async () => {
-            await sdk.actions.ready();
-            console.log(sdk)
+            setContext(await sdk.context);
+            sdk.actions.ready();
         };
         if (sdk && !isSDKLoaded) {
             setIsSDKLoaded(true);
             load();
         }
     }, [isSDKLoaded]);
+
+    if (!isSDKLoaded) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <div>
             <JackpotGame />
