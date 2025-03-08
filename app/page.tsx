f0fdf0c1165d1577c872a00c71709607dd390976
daksh <akshd@outlook.com>
2025-03-08 16:56:59 +0530

    chore: fix the game wrapper

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import GameWrapper from "@/components/game-wrapper";
 import { Metadata } from "next";
 import dynamic from "next/dynamic";
 
@@ -37,17 +38,7 @@ export async function generateMetadata(): Promise<Metadata> {
 
 
 export default function Page() {
-  // return (
-  //   <div className={`flex min-h-screen flex-col items-center justify-center p-4 text-center bg-gray-900 text-white ${inter.className}`}>
-  //     <h1 className={`text-4xl font-bold mb-4 ${montserrat.className} text-blue-400`}>Under Maintenance</h1>
-  //     <p className="text-xl mb-8 text-gray-200">We're currently performing some updates to improve your experience.</p>
-  //     <div className="flex flex-col gap-4">
-  //       <p className="text-gray-300">We'll be back soon!</p>
-  //       <p className="text-sm text-gray-400">Thank you for your patience.</p>
-  //     </div>
-  //   </div>
-  // )
   return (
-    <JackpotGame />
+    < GameWrapper />
   )
 }
